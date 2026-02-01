5dcf9620f26a5e29b8ce6d29e82d3470616bec48
Akan Çengel <akancengel@Akan-Macbook.local>
2026-02-01 22:23:01 +0300

    NETSPACE BETA 0.1.1

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -1,13 +1,7 @@
 import { buildApp } from '../src/app';
-import type { FastifyInstance } from 'fastify';
-
-let app: FastifyInstance | null = null;
 
 export default async function handler(req: any, res: any) {
-    if (!app) {
-        app = await buildApp();
-        await app.ready();
-    }
-
+    const app = await buildApp();
+    await app.ready();
     app.server.emit('request', req, res);
 }
